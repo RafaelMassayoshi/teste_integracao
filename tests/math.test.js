b76b954c4ff1b2b662bd2eaf5606438e61128df5
RafaelMassayoshi <rafaeltargaeda@hotmail.com>
2026-03-03 00:51:03 +0000

    Opreaçoes matematicas adicionads

--- tests/math.test.js
+++ tests/math.test.js
@@ -3,3 +3,14 @@ const math = require("../math");
 test('soma 2 + 3 = 5', ()=>{
     expect(math.add(2,3)).toBe(5);
 })
+test('subtracao 3 - 2 = 1', ()=>{
+    expect(math.subtract(3, 2)).toBe(1);
+})
+test('multiplicação 2 x 2 = 4', ()=>{
+    expect(math.multply(2, 2)).toBe(4);
+})
+test('dividido 9 / 3 = 3', ()=>{
+    expect(math.divide(9,3)).toBe(3);
+})
+
+
